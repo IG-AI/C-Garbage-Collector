652d46299f1d4156a797f1b7176615b551c20944
AdamInersjo <adam.inersjo@hotmail.com>
2016-12-06 20:19:05 +0100

    Change documentation to JavaDoc style

--- header.h
+++ header.h
@@ -1,17 +1,36 @@
+/**
+ *  @file  header.h
+ *  @brief A module for creating headers (meta data) for data and structures.
+ *
+ *  @author Daniel Agstrand
+ *  @author Henrik Bergendal
+ *  @author Adam Inersjo
+ *  @author Maria Lindqvist
+ *  @author Simon Pellgard
+ *  @author Robert Rosborg
+ */
+
+#include <stdlib.h>
 
 #ifndef __header__
 #define __header__
 
-/// creates a header and saves it on the heap
-/// \param format_string
-/// \param heap_ptr the place on the heap where the header will be saved
-void create_struct_header(char *format_string, void* heap_ptr);
+/**
+ *  @brief Creates a header and saves it on the heap
+ *
+ *  @param format_string
+ *  @param heap_ptr the place on the heap where the header will be saved
+ */
+void create_struct_header(char *format_string, void *heap_ptr);
 
 
-/// creates a header and saves it on the heap
-/// \param bytes the size in bytes
-/// \param heap_ptr the place on the heap where the header will be saved
-void create_data_header(size_t bytes, void* heap_ptr);
+/**
+ *  @brief Creates a header and saves it on the heap
+ *
+ *  @param bytes the size in bytes
+ *  @param heap_ptr the place on the heap where the header will be saved
+ */
+void create_data_header(size_t bytes, void *heap_ptr);
 
 
 #endif
--- stack_search.h
+++ stack_search.h
@@ -1,15 +1,29 @@
-
+/**
+ *  @file  stack_search.h
+ *  @brief A module for searching through the stack.
+ *
+ *  @author Daniel Agstrand
+ *  @author Henrik Bergendal
+ *  @author Adam Inersjo
+ *  @author Maria Lindqvist
+ *  @author Simon Pellgard
+ *  @author Robert Rosborg
+ */
 
 #ifndef __stack_search__
 #define __stack_search__
 
-/// finds a possible pointer on the stack
-/// \param stack_top the top of the stack to seach
-/// \param stack_bottom the bottom of the stack to seach
-/// \param heap_start the start of the heap
-/// \param heap_end the end of the heap
-/// \return a possible pointer into the heap
-void *stack_find_next_ptr(void **stack_top, void *stack_bottom, void *heap_start, void *heap_end);
+/**
+ *  @brief Finds a possible pointer on the stack.
+ *
+ *  @param  stack_top the top of the stack to seach
+ *  @param  stack_bottom the bottom of the stack to seach
+ *  @param  heap_start the start of the heap
+ *  @param  heap_end the end of the heap
+ *  @return a possible pointer into the heap
+ */
+void *stack_find_next_ptr(void **stack_top, void *stack_bottom,
+                          void *heap_start, void *heap_end);
 
 
 
