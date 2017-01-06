98c5806329bb08e67d1d5f0f4ef4a10c9a3c68aa
Adam Inersjo <adam.inersjo@hotmail.com>
2017-01-06 15:56:14 +0100

    Fix so that GC works on Solaris x86

--- header_test.c
+++ header_test.c
@@ -1663,7 +1663,7 @@ main(void)
     }
   
   // ******************** RUN TESTS ***************** //
-  CU_basic_set_mode(CU_BRM_VERBOSE);
+  //CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
   CU_cleanup_registry();
   return CU_get_error();
